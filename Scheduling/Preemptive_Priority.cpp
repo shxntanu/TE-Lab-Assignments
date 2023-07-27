475b6e0d6ba15176bd129c64f335707a6cabd7ae
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-07-27 09:55:18 +0530

    Completed Priority Scheduling (Preemptive)

--- Scheduling/Preemptive_Priority.cpp
+++ Scheduling/Preemptive_Priority.cpp
@@ -1,8 +1,11 @@
+/*
+Author: Shantanu Wable
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-class Process
-{
+class Process {
 public:
     int id;
     int burst;
@@ -10,7 +13,7 @@ public:
     int priority; // Lower Number -> Higher Priority
 };
 
-void waitingTime(Process processes[], int n, int wt[], int ct[], int tat[])
+void pPriority(Process processes[], int n, int wt[], int ct[], int tat[])
 {
     int remainingTime[n];
 
@@ -60,7 +63,8 @@ int main () {
     Process proc[] = { {1, 11, 0, 2}, {2, 28, 5, 0}, {3, 2, 12, 3}, {4, 10, 2, 1}, {5, 16, 9, 4} };
     int n = sizeof(proc) / sizeof(proc[0]);
     int wt[n], ct[n], tat[n];
-    waitingTime(proc, n, wt, ct, tat);
+    
+    pPriority(proc, n, wt, ct, tat);
 
     cout.width(5);
     cout<<"ID";
