ec5861ee95336c79f29681b46240c704e2128afc
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-07-27 09:48:24 +0530

    Completed NP_Priority.cpp

--- Scheduling/NP_Priority.cpp
+++ Scheduling/NP_Priority.cpp
@@ -14,7 +14,7 @@ public:
     int priority; // Lower Number -> Higher Priority
 };
 
-void waitingTime(Process processes[], int n, int wt[], int ct[], int tat[])
+void npPriority(Process processes[], int n, int wt[], int ct[], int tat[])
 {
     int remainingTime[n];
 
@@ -53,7 +53,8 @@ int main () {
     Process proc[] = { {1, 11, 0, 2}, {2, 28, 5, 0}, {3, 2, 12, 3}, {4, 10, 2, 1}, {5, 16, 9, 4} };
     int n = sizeof(proc) / sizeof(proc[0]);
     int wt[n], ct[n], tat[n];
-    waitingTime(proc, n, wt, ct, tat);
+    
+    npPriority(proc, n, wt, ct, tat);
 
     cout.width(5);
     cout<<"ID";
@@ -82,4 +83,6 @@ int main () {
         cout.width(5);
         cout<<wt[i]<<endl;
     }     
+
+    return 0;
 }
