b8d9fc2a978770b3487cbe4557cd1e593cf4dd2b
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-07-31 10:07:38 +0530

    Update init.sql

--- 02/init.sql
+++ 02/init.sql
@@ -7,12 +7,21 @@ a. Design and Develop SQLDDL statements which demonstrate the use of SQL objects
 b. Write at least 10 SQL queries on the suitable database application using SQL DML statements.
 */
 
+/*
+Author: Shantanu Wable
+
+Assignment 2: SQL Queries
+
+a. Design and Develop SQLDDL statements which demonstrate the use of SQL objects such as Table, View, Index, Sequence, Synonym, different constraints etc. 
+b. Write at least 10 SQL queries on the suitable database application using SQL DML statements.
+*/
+
 
 USE 31380_db;
 
-DROP TABLE dept;
 DROP TABLE project;
 DROP TABLE employee;
+DROP TABLE dept;
 DROP VIEW BasicDeets;
 
 CREATE TABLE dept (
@@ -30,15 +39,21 @@ CREATE TABLE project (
 	proj_cost INT,
 	proj_year INT,
 	PRIMARY KEY (proj_id),
-	FOREIGN KEY (dept_id) REFERENCES dept(dept_id) ON DELETE CASCADE
 );
+
+ALTER TABLE project 
+	ADD CONSTRAINT `prfk_1` 	
+	FOREIGN KEY (dept_id) 
+	REFERENCES dept(dept_id) 
+	ON DELETE CASCADE
+;
 
 CREATE TABLE employee ( 
 	emp_id INT,
 	dept_id INT,
 	emp_fname VARCHAR(20),
 	emp_lname VARCHAR(20),
-	emp_position VARCHAR(20),
+	emp_position VARCHAR(50),
 	emp_salary INT,
 	emp_jdate DATE,
 	PRIMARY KEY (emp_id)
