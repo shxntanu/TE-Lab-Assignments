2d79aa36ab2c12705041a683244aa1dcbae2195c
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-08-02 09:33:55 +0530

    Update 01-queries.sql

--- 03/01-queries.sql
+++ 03/01-queries.sql
@@ -11,4 +11,26 @@ SELECT emp.emp_id AS "Employee ID",
 	FROM employee AS emp
 	NATURAL JOIN dept AS dep
 ;
-	
+
+SELECT emp.emp_fname AS "Name",
+	emp.emp_position AS "Position",
+	dep.dept_loc AS "Location",
+	emp.emp_jdate AS "Joining Date",
+	dep.dept_name AS "Department"
+	FROM employee AS emp
+	NATURAL JOIN dept AS dep
+	WHERE emp.dept_id = dep.dept_id
+;
+
+SELECT emp.emp_id AS "Employee ID",
+	emp.emp_fname AS "Name",
+	emp.emp_lname AS "Surname",
+	emp.emp_position AS "Position",
+	emp.emp_salary AS "Salary",
+	emp.emp_jdate AS "Joining Date",
+	pr.proj_id AS "Project ID",
+	pr.proj_cost AS "Project Cost"
+	FROM employee AS emp
+	NATURAL JOIN project as pr
+	WHERE (pr.proj_location != "Hyderabad") AND (emp.dept_id = pr.dept_id)
+;
