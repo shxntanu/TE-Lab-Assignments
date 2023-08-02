5ce9bd511c5c9eacb9a6a959eef8886a0cf1ce7b
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-08-02 09:17:16 +0530

    Create 01-queries.sql

---
+++ 03/01-queries.sql
@@ -0,0 +1,14 @@
+USE 31380_db;
+
+SELECT emp.emp_id AS "Employee ID",
+	emp.emp_fname AS "Name",
+	emp.emp_lname AS "Surname",
+	emp.emp_position AS "Position",
+	dep.dept_name AS "Department",
+	dep.dept_loc AS "Dept. Location",
+	emp.emp_salary AS "Salary",
+	emp.emp_jdate AS "Joining Date"
+	FROM employee AS emp
+	NATURAL JOIN dept AS dep
+;
+	
