aeec12040c9a3734c0f8c25c42e79dfaac88cbf3
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-08-05 22:51:09 +0530

    Update RoundRobin.cpp

--- Scheduling/RoundRobin.cpp
+++ Scheduling/RoundRobin.cpp
@@ -41,10 +41,13 @@ void RoundRobin(Process processes[], int n, int ts, int wt[], int ct[], int tat[
         int cntr = 0; // Counter to keep track of time quantum spent on the current process
 
         // Execute the process for a time quantum (ts) or its remaining burst time (whichever is smaller)
+        // Increment the counter for the time spent on the current process
+        // Increment the current time
+        
         while (cntr != ts && cntr != remainingTime[curr]) {
             
-            currentTime++; // Increment the current time
-            cntr++; // Increment the counter for the time spent on the current process
+            currentTime++; 
+            cntr++; 
 
             // Check if any new processes have arrived at the current time and add them to the Ready Queue
             for (int j = 0; j < n; j++) {
@@ -56,15 +59,21 @@ void RoundRobin(Process processes[], int n, int ts, int wt[], int ct[], int tat[
         remainingTime[curr] -= cntr; // Reduce the remaining burst time of the current process by the time quantum spent on it
 
         // If the remaining burst time of the current process becomes zero, the process is completed
+        // Set the completion time of the current process
+        // Calculate the turnaround time for the current process
+        // Increment the number of completed tasks
+        
         if (remainingTime[curr] == 0) {
-            ct[curr] = currentTime; // Set the completion time of the current process
-            tat[curr] = ct[curr] - processes[curr].arrival; // Calculate the turnaround time for the current process
-            completedTasks++; // Increment the number of completed tasks
+            ct[curr] = currentTime; 
+            tat[curr] = ct[curr] - processes[curr].arrival; 
+            completedTasks++; 
             wt[curr] = ct[curr] - processes[curr].arrival - processes[curr].burst;
         }
             
         else {
-            RQ.push(curr); // If the process is not completed, push it back to the Ready Queue to be scheduled later
+            // If the process is not completed, push it back to the Ready Queue to be scheduled later
+            
+            RQ.push(curr); 
         }
     }
 }
@@ -76,11 +85,9 @@ int main() {
     int wt[n], ct[n], tat[n];
     for(int i=0;i<n;i++)
         wt[i] = 0;
-
-    // Call the RoundRobin function to calculate waiting time, completion time, and turnaround time for each process
+    
     RoundRobin(proc, n, 3, wt, ct, tat);
-
-    // Print the table header
+    
     cout.width(5);
     cout << "ID";
     cout.width(5);
@@ -93,8 +100,7 @@ int main() {
     cout << "TAT";
     cout.width(5);
     cout << "WT" << endl;
-
-    // Print process details along with their arrival time, burst time, completion time, turnaround time, and waiting time
+    
     for (int i = 0; i < n; i++)
     {
         cout.width(5);
