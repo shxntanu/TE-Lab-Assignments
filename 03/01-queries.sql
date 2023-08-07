b56477d93bdde0ec1d4f2a014abd06a17ce52035
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-08-07 09:54:00 +0530

    Update 01-queries.sql

--- 03/01-queries.sql
+++ 03/01-queries.sql
@@ -1,5 +1,6 @@
 USE 31380_db;
 
+-- Natural Join
 SELECT emp.emp_id AS "Employee ID",
 	emp.emp_fname AS "Name",
 	emp.emp_lname AS "Surname",
@@ -8,29 +9,62 @@ SELECT emp.emp_id AS "Employee ID",
 	dep.dept_loc AS "Dept. Location",
 	emp.emp_salary AS "Salary",
 	emp.emp_jdate AS "Joining Date"
-	FROM employee AS emp
-	NATURAL JOIN dept AS dep
+FROM employee AS emp
+NATURAL JOIN dept AS dep
 ;
 
+-- Left Outer Join
 SELECT emp.emp_fname AS "Name",
 	emp.emp_position AS "Position",
 	dep.dept_loc AS "Location",
 	emp.emp_jdate AS "Joining Date",
 	dep.dept_name AS "Department"
-	FROM employee AS emp
-	NATURAL JOIN dept AS dep
-	WHERE emp.dept_id = dep.dept_id
+	FROM employee as emp
+LEFT JOIN dept as dep
+ON emp.dept_id = dep.dept_id
 ;
 
-SELECT emp.emp_id AS "Employee ID",
-	emp.emp_fname AS "Name",
-	emp.emp_lname AS "Surname",
-	emp.emp_position AS "Position",
-	emp.emp_salary AS "Salary",
-	emp.emp_jdate AS "Joining Date",
-	pr.proj_id AS "Project ID",
-	pr.proj_cost AS "Project Cost"
-	FROM employee AS emp
-	NATURAL JOIN project as pr
-	WHERE (pr.proj_location != "Hyderabad") AND (emp.dept_id = pr.dept_id)
+-- Right Outer Join
+SELECT
+    emp.emp_id AS "Employee ID",
+    emp.emp_fname AS "Name",
+    emp.emp_lname AS "Surname",
+    emp.emp_position AS "Position",
+    emp.emp_salary AS "Salary",
+    emp.emp_jdate AS "Joining Date",
+    pr.proj_cost AS "Project Cost"
+FROM employee AS emp
+RIGHT JOIN project as pr
+    ON (pr.proj_location != "Hyderabad") AND (emp.dept_id = pr.dept_id)
+GROUP BY emp.emp_id;
+
+
+SELECT
+	dep.dept_name AS "Dept. Name",
+	emp.emp_fname AS "Employee Name",
+	emp.emp_position AS "Position"
+FROM employee AS emp
+LEFT JOIN dept AS dep
+project AS pr
+WHERE
+	((dep.dept_id = emp.dept_id) AND
+	(dep.dept_id = pr.dept_id)) AND
+	pr.proj_year = 2020
+; 
+
+SELECT 
+	edj.emp_position AS "Position",
+	edj.dept_name AS "Dept. Name"
+FROM (
+	SELECT *
+	FROM employee
+	NATURAL JOIN dept
+	WHERE employee.dept_id = dept.dept_id
+) AS edj
+RIGHT JOIN project AS pr
+ON 
+	edj.dept_id = pr.dept_id
+WHERE 
+	pr.proj_cost > 30000
 ;
+	
