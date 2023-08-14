14cbfaa791931bcdeb6ca307e81f69e9eedf64ad
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-08-14 10:49:10 +0530

    Update 01-plsql.sql

--- 04/01-plsql.sql
+++ 04/01-plsql.sql
@@ -16,41 +16,39 @@ CREATE TABLE fine (
 	FOREIGN KEY (fk_br_id) REFERENCES borrower(pk_br_id) ON DELETE CASCADE
 );
 
---ACCEPT roll INT PROMPT 'Enter Roll Number: ';
---ACCEPT book VARCHAR(50) PROMPT 'Enter book name: ';
---ACCEPT doi DATE PROMPT 'Enter date of issue (YYYY-MM-DD): ';
--- INSERT INTO fine (fk_br_id, dt, amount) VALUES (r_no, dt, fine);
+-- 	DECLARE exitHandler HANDLER FOR NOT FOUND dbms_output.put_line('No such entry!');
 
-DECLARE
-	r_no borrower.pk_br_id%type;
-	bk borrower.bk_name%type;
-	dt borrower.issuedate%type;
-	fine fine.amount%type;
-	status fine.status%type;
-	dt2 INT;
+DELIMITER $$
+CREATE PROCEDURE library (IN r_no INT, IN bk VARCHAR(50), IN dt DATE)
 BEGIN
-	r_no := :r_no;
-	bk := :bk;
-	dt := :dt;
-	dt2 := DATEDIFF(dt, SELECT issuedate FROM borrower WHERE (pk_br_id = r_no AND bk_name = bk));
-	status := SELECT status FROM borrower WHERE (pk_br_id = r_no AND bk_name = bk);
-	IF status = False THEN
+	DECLARE fine INT;
+	DECLARE dt2 INT;
+	
+	SELECT @idt := issuedate 
+		FROM borrower 
+		WHERE (pk_br_id = r_no AND bk_name = bk);
+	SELECT @stt := status 
+		FROM borrower 
+		WHERE (pk_br_id = r_no AND bk_name = bk);
+	SET dt2 := DATEDIFF(dt, @idt);
+	IF @stt = False THEN
 		IF dt2 BETWEEN 0 AND 14 THEN
-			fine := 0;
+			SET fine := 0;
 		ELSEIF dt2 BETWEEN 15 AND 30 THEN
-			fine := 5 * dt2;
-		ELSEIF 
-			fine := 50 * dt2;
+			SET fine := 5 * dt2;
+		ELSE
+			SET fine := 50 * dt2;
 		END IF;
 		INSERT INTO fine (fk_br_id, dt, amount) VALUES (r_no, dt, fine);
 		UPDATE borrower 
 			SET status = True
 			WHERE (pk_br_id = r_no AND bk_name = bk);
 	END IF;
-	EXCEPTION  
-	   	WHEN no_data_found THEN  
-			dbms_output.put_line('No such entry!');  
-	   	WHEN others THEN  
-		  	dbms_output.put_line('Error!');  
-END;
-/
+END $$
+DELIMITER ;
+
+	
+	
+	
+	
+	
