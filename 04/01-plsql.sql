5764023004e817b747f685f3357dfd68003195c9
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-08-15 23:58:41 +0530

    Update 01-plsql.sql

--- 04/01-plsql.sql
+++ 04/01-plsql.sql
@@ -1,21 +1,5 @@
 USE 31380_db;
 
-CREATE TABLE borrower (
-	pk_br_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	br_name VARCHAR(50),
-	issuedate DATE,
-	bk_name VARCHAR(50),
-	status BOOL
-);
-
-CREATE TABLE fine (
-	pk_f_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	fk_br_id INT,
-	finedate DATE,
-	amount INT,
-	FOREIGN KEY (fk_br_id) REFERENCES borrower(pk_br_id) ON DELETE CASCADE
-);
-
 -- 	DECLARE exitHandler HANDLER FOR NOT FOUND dbms_output.put_line('No such entry!');
 
 DELIMITER $$
