fd7e4809d9715cf33c8f1e2aad6527e169931081
Shantanu Wable <shantanuwable2003@gmail.com>
2023-09-01 00:38:08 +0530

    Added a second test case program

---
+++ Macro-Assembler/Pass-1/input/program2.asm
@@ -0,0 +1,15 @@
+MACRO
+M1 &X, &Y, &A=AREG, &B=
+MOVER &A, &X
+ADD &A, =’1’
+MOVER &B, &Y
+ADD &A, =’5’
+MEND
+
+MACRO
+M2 &P, &Q, &U=CREG, &V=DREG
+MOVER &U, &P
+MOVER &V, &Q
+ADD &U, =’15’
+ADD &V, =’10’
+MEND
