adc49a2efe443354d5350751a71d8e4c2634e5cd
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-09-14 10:27:20 +0530

    Create mdt.asm

---
+++ Macro-Assembler/Pass-2/input/mdt.asm
@@ -0,0 +1,12 @@
+MOVER (P,3) (P,1) 
+ADD (P,3) (P,2) 
+MOVEM (P,3) (P,1) 
+MEND 
+MOVER (P,3) (P,1) 
+ADD (P,3) (P,2) 
+MOVEM (P,3) (P,1) 
+MEND 
+MOVER (P,3) (P,1) 
+ADD (P,3) (P,2) 
+MOVEM (P,3) (P,1) 
+MEND 
