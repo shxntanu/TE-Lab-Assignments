5c26f35954d03413c9bfae9e8a6cd07413d035fb
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-10-26 09:49:55 +0530

    💿 Broadcast Server

---
+++ Socket-Programming/Chat-Server/server.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'  # Server's IP address
+PORT = 8080
+
+clients = []
+
+def broadcast(message, sender_socket):
+    for client in clients:
+        if client != sender_socket:
+            try:
+                client.send(message)
+            except:
+                clients.remove(client)
+
+def client_handler(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024)
+            if not message:
+                break
+            broadcast(message, client_socket)
+        except:
+            break
+    client_socket.close()
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(5)
+    print(f'Server listening on {HOST}:{PORT}')
+
+    while True:
+        client_socket, addr = server.accept()
+        print(f'Accepted connection from {addr[0]}:{addr[1]}')
+        clients.append(client_socket)
+        client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+        client_thread.start()
+
+if __name__ == '__main__':
+    main()
