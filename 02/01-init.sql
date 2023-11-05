3ac2451452872bd027a21833fe0d051266462778
Shantanu <shantanuwable2003@gmail.com>
2023-11-05 20:25:08 +0530

    Cleaned code

--- 02/01-init.sql
+++ 02/01-init.sql
@@ -3,59 +3,35 @@ Author: Shantanu Wable
 
 Assignment 2: SQL Queries
 
-a. Design and Develop SQLDDL statements which demonstrate the use of SQL objects such as Table, View, Index, Sequence, Synonym, different constraints etc. 
+a. Design and Develop SQL DDL statements which demonstrate the use of SQL objects such as Table, View, Index, Sequence, Synonym, different constraints etc. 
 b. Write at least 10 SQL queries on the suitable database application using SQL DML statements.
 */
 
 USE 31380_db;
 
-DROP TABLE project;
-DROP TABLE employee;
-DROP TABLE dept;
-DROP VIEW BasicDeets;
-
 CREATE TABLE dept (
-	dept_id INT,
+	dept_id INT PRIMARY KEY,
 	dept_name VARCHAR(50),
-	dept_loc VARCHAR(50),
-	PRIMARY KEY(dept_id)
+	dept_loc VARCHAR(50)
 );
 
 CREATE TABLE project (
-	proj_id INT,
+	proj_id INT PRIMARY KEY,
 	dept_id INT, 
 	proj_name VARCHAR(50),
 	proj_location VARCHAR(50),
 	proj_cost INT,
 	proj_year INT,
-	PRIMARY KEY (proj_id)
+	FOREIGN KEY (dept_id) REFERENCES dept(dept_id) ON DELETE CASCADE
 );
 
-ALTER TABLE project 
-	ADD CONSTRAINT `prfk_1` 	
-	FOREIGN KEY (dept_id) 
-	REFERENCES dept(dept_id) 
-	ON DELETE CASCADE
-;
-
 CREATE TABLE employee ( 
-	emp_id INT,
+	emp_id INT PRIMARY KEY,
 	dept_id INT,
 	emp_fname VARCHAR(20),
 	emp_lname VARCHAR(20),
 	emp_position VARCHAR(50),
 	emp_salary INT,
 	emp_jdate DATE,
-	PRIMARY KEY (emp_id)
+	FOREIGN KEY (dept_id) REFERENCES dept(dept_id) ON DELETE CASCADE
 );
-
-DESCRIBE employee;
-
-ALTER TABLE employee 
-	ADD CONSTRAINT `emfk_1` 	
-	FOREIGN KEY (dept_id) 
-	REFERENCES dept(dept_id) 
-	ON DELETE CASCADE
-;
-
-DESCRIBE employee;
