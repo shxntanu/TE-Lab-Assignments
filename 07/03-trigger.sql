447ac64d80255b8d6be2a1d37ab385dd0bca7d37
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-11-06 09:11:38 +0530

    Update 03-trigger.sql

--- 07/03-trigger.sql
+++ 07/03-trigger.sql
@@ -2,13 +2,13 @@ USE 31380_db;
 
 DELIMITER $$
 CREATE TRIGGER update_trig
-	BEFORE UPDATE ON Library
-	FOR EACH ROW
-	BEGIN
-	
+BEFORE UPDATE ON Library
+FOR EACH ROW
+BEGIN
+
 	INSERT INTO Library_Audit (book_id, old_book_name, old_isbn, old_page_count, old_author, old_year,old_copies_sold) VALUES 
-		(OLD.book_id, OLD.book_name, OLD.isbn, OLD.page_count, OLD.author, OLD.year, OLD.copies_sold);
-	
+	(OLD.book_id, OLD.book_name, OLD.isbn, OLD.page_count, OLD.author, OLD.year, OLD.copies_sold);
+
 END $$
 DELIMITER ;
 
