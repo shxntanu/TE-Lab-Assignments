1931e4325cbd2f2fb5a0d3ca7df1265af9b9de00
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-11-07 15:23:45 +0530

    Update and rename 01-init.sql to main.sql

--- 07/main.sql
+++ 07/main.sql
@@ -43,3 +43,35 @@ INSERT INTO Library VALUES
 	(19,'qui',67772169,114,'expedita','2006-01-31',35066713),
 	(20,'quae',78427689,222,'quaerat','1992-07-22',54087575)
 ;
+
+DELIMITER $$
+CREATE TRIGGER update_trig
+BEFORE UPDATE ON Library
+FOR EACH ROW
+BEGIN
+
+	INSERT INTO Library_Audit (book_id, old_book_name, old_isbn, old_page_count, old_author, old_year,old_copies_sold) VALUES 
+	(OLD.book_id, OLD.book_name, OLD.isbn, OLD.page_count, OLD.author, OLD.year, OLD.copies_sold);
+
+END $$
+DELIMITER ;
+
+-- Testing the trigger
+
+UPDATE Library
+SET book_name = 'haha totam' WHERE book_id = 1;
+
+DELIMITER $$
+CREATE TRIGGER error_trig
+BEFORE UPDATE ON Library
+FOR EACH ROW
+BEGIN
+	IF NEW.book_name = OLD.book_name THEN
+	SIGNAL SQLSTATE '45000'
+	SET message_text = 'Same value of updated book';
+	END IF;
+END $$
+DELIMITER ; 
+
+UPDATE Library
+SET book_name = 'quae' WHERE book_id = 3;
