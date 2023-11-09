01f199ab5aee9fdb3e62b436d353e3764f15e76b
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-11-09 08:00:55 +0530

    Update javamongo.java

--- JDBC MongoDB/javamongo.java
+++ JDBC MongoDB/javamongo.java
@@ -1,22 +1,18 @@
 package mongoJava;
 
+import java.util.*;
+import com.mongodb.client.*;
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import java.util.Scanner;
-import com.mongodb.MongoCredential;
 
+public class mongo {
+    public static void main(String[] args) {
+        MongoClient client = new MongoClient("127.0.0.1", 27017);
+        System.out.println("Connected to DB");
+        MongoDatabase db = client.getDatabase("31380_db");
+        MongoCollection<Document> collection = db.getCollection("sampleCollection");
 
-public class javamongo {
-	public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient("10.10.11.238", 27017);
-        MongoCredential credential; 
-        credential = MongoCredential.createCredential("te31375", "31375_db", "te31375".toCharArray()); 
-        System.out.println("Connected to the database successfully");// Connect to MongoDB
-        MongoDatabase database = mongoClient.getDatabase("31375_db"); // Replace with your database name
-        MongoCollection<Document> collection = database.getCollection("sampleCollection"); // Replace with your collection name
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         while (true) {
             System.out.println("Menu:");
@@ -26,72 +22,74 @@ public class javamongo {
             System.out.println("4. Delete");
             System.out.println("5. Exit");
             System.out.print("Enter your choice: ");
-            int choice = scanner.nextInt();
+            int choice = sc.nextInt();
 
             switch (choice) {
                 case 1:
-                    // Create operation
-                    createDocument(collection, scanner);
+                    createDocument(collection, sc);
                     break;
+
                 case 2:
-                    // Read operation
                     readDocuments(collection);
                     break;
+
                 case 3:
-                    // Update operation
-                    updateDocument(collection, scanner);
+                    updateDocument(collection, sc);
                     break;
+
                 case 4:
-                    // Delete operation
-                    deleteDocument(collection, scanner);
-                    break;
-                case 5:
-                    System.out.println("Exiting the program.");
-                    mongoClient.close(); // Close the MongoDB connection
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please select a valid option.");
+                    deleteDocument(collection, sc);
                     break;
             }
+
+            System.out.println("Continue? (y/n): ");
+            String ans = sc.next();
+            if (ans.equals("N") || ans.equals("n"))
+                break;
         }
-	}
-private static void createDocument(MongoCollection<Document> collection, Scanner scanner) {
-    System.out.print("Enter a key: ");
-    String key = scanner.next();
-    System.out.print("Enter a value: ");
-    String value = scanner.next();
-    Document document = new Document(key, value);
-    collection.insertOne(document);
-    System.out.println("Document added successfully.");
-}
-
-private static void readDocuments(MongoCollection<Document> collection) {
-    for (Document doc : collection.find()) {
-        System.out.println(doc.toJson());
+        sc.close();
+        client.close();
     }
-}
 
-private static void updateDocument(MongoCollection<Document> collection, Scanner scanner) {
-	System.out.print("Enter the key of the document to update: ");
-	String key = scanner.next();
-	System.out.print("Enter the previous value: ");
-	String value = scanner.next();
-	Document filter = new Document(key, value);
-	System.out.print("Enter the new value: ");
-	String newValue = scanner.next();
-	Document update = new Document("$set", new Document(key, newValue));
-	collection.updateMany(filter, update);
-	System.out.println("Document updated successfully.");
-}
+    public static void createDocument(MongoCollection<Document> collection, Scanner sc) {
+        System.out.println("Enter a key: ");
+        String key = sc.next();
+        System.out.println("Enter its value: ");
+        String value = sc.next();
+        Document newdoc = new Document(key, value);
+        collection.insertOne(newdoc);
+        System.out.println("Document inserted successfully");
 
-private static void deleteDocument(MongoCollection<Document> collection, Scanner scanner) {
-    System.out.print("Enter the key of the document to delete: ");
-    String key = scanner.next();
-    System.out.print("Enter the value of the document to delete: ");
-    String value = scanner.next();
-    Document filter = new Document(key, value);
-    collection.deleteOne(filter);
-    System.out.println("Document deleted successfully.");
-}
+    }
+
+    public static void readDocuments(MongoCollection<Document> collection) {
+        for (Document doc : collection.find()) {
+            System.out.println(doc.toJson());
+        }
+    }
+
+    public static void updateDocument(MongoCollection<Document> collection, Scanner sc) {
+        System.out.println("Enter document identifier key: ");
+        String key = sc.next();
+        System.out.println("Enter corresponding value");
+        String value = sc.next();
+        Document filter = new Document(key, value);
+        System.out.println("Enter new value");
+        String uValue = sc.next();
+        Document uDoc = new Document(key, uValue);
+        collection.updateOne(filter, uDoc);
+        System.out.println("Document updated successfully");
+    }
+
+    public static void deleteDocument(MongoCollection<Document> collection, Scanner sc) {
+        System.out.println("Enter document identifier key: ");
+        String key = sc.next();
+        System.out.println("Enter corresponding value");
+        String value = sc.next();
+        Document filter = new Document(key, value);
+        collection.deleteOne(filter);
+        System.out.println("Document deleted successfully");
+
+    }
+
 }
