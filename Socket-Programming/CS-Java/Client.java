a543da9719c173beb7843ce6b141babfb8504cd9
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-11-20 23:12:03 +0530

    Update Client.java

--- Socket-Programming/CS-Java/Client.java
+++ Socket-Programming/CS-Java/Client.java
@@ -1,28 +1,30 @@
 import java.net.*;
 import java.io.*;
-import java.util.*;
 
-public class C{
-    public static void main(String [] args){
-        try{
-            while(true){
-                Socket sock = new Socket("localhost", 21);
-                PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
-                Scanner in = new Scanner(sock.getInputStream());
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket sock = new Socket("localhost", 8888);
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
+
+            String userInput;
+            while ((userInput = userIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("Server: " + in.readLine());
+
+                // Optionally handle server's response
 
                 System.out.print("Reply: ");
-                Scanner temp = new Scanner(System.in);
-                String reply;
-                reply = temp.nextLine();
-                
-                out.println(reply);
-                System.out.println("Server: " + in.nextLine());
-                
-                sock.close();
             }
+
+            out.close();
+            in.close();
+            userIn.close();
+            sock.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
