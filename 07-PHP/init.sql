00b15054e95c0fe056fd38869990587ebcb30c08
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2024-04-11 19:47:24 +0530

    Create init.sql

---
+++ 07-PHP/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS users_db;
+USE users_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    age INT
+);
